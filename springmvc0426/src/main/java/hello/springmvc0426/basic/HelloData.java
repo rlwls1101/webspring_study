197e005360abecc53a9c5bb3763c7eaac9f28b27
leegijin <rlwls1102@gmail.com>
2021-05-06 16:49:50 +0900

    requestBody

---
+++ springmvc0426/src/main/java/hello/springmvc0426/basic/HelloData.java
@@ -0,0 +1,9 @@
+package hello.springmvc0426.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
---
+++ springmvc0426/src/main/java/hello/springmvc0426/basic/response/responseViewController.java
@@ -0,0 +1,9 @@
+package hello.springmvc0426.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class responseViewController {
+
+}
