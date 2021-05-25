6c4e3408d476364294d7327d82a7743e7afbfe1b
leegijin <rlwls1102@gmail.com>
2021-05-25 15:28:36 +0900

    add entity

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String name;
+    @Embedded
+    private Address address;
+    @OneToMany(mappedBy = "member") // 나는 order쪽에서 member 값을 가져왔어 라는뜻
+    private List<Order> orders = new ArrayList<>();
+}
--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.Member;
-import jpabook.jpashop.MemberRepository;
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-import javax.persistence.EntityManager;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
@@ -24,8 +23,7 @@ public class MemberRepositoryTest {
         Long savedId = memberRepository.save(member); Member findMember = memberRepository.find(savedId);
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
 
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername())
-        ;
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
     }
 }
